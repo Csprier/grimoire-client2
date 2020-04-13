92939aa9012e0505141fb36ba9e01a9825115a60
Cam Prier <CSPrier@gmail.com>
2020-04-13 00:38:19 -0400

    added some style to the login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,33 +12,40 @@ const Login = () => {
 
   return(
     <form className="login-form" onSubmit={handleSubmit}>
-      <label className="label">Username</label>
-      <input 
-        className="input"
-        type="text"
-        name="username"
-        onChange={handleChange}
-        value={values.username || ''}
-        placeholder="Username..."
-        required
-      />
-      {errors.username && (
-        <p className="error">{errors.username}</p>
-      )}
-      <label className="label">Password</label>
-      <input 
-        className="input"
-        type="password"
-        name="password"
-        onChange={handleChange}
-        value={values.password  || ''}
-        placeholder="Password..."
-        required
-      />
-      {errors.password && (
-        <p className="error">{errors.password}</p>
-      )}
-      <button type="submit" className="login-button">Login</button>
+      <h1>Login</h1>
+      <div className="input-container">
+        <label className="label">Username
+          <input 
+            className="input"
+            type="text"
+            name="username"
+            onChange={handleChange}
+            value={values.username || ''}
+            placeholder="Username..."
+            required
+          />
+          {errors.username && (
+            <p className="error">{errors.username}</p>
+          )}
+        </label>
+        <label className="label">Password
+          <input 
+            className="input"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={values.password  || ''}
+            placeholder="Password..."
+            required
+          />
+          {errors.password && (
+            <p className="error">{errors.password}</p>
+          )}
+        </label>
+      </div>
+      <div className="button-container">
+        <button type="submit" className="login-button">Login</button>
+      </div>
     </form>
   );
 }
