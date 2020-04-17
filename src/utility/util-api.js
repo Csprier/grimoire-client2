1b90906ebd8826f71f7eb3b171303eac4033616e
Cam Prier <CSPrier@gmail.com>
2020-04-17 15:23:31 -0400

    Changed api-config.js to just be localhost instead of process.env to avoid fetch failures

--- src/utility/util-api.js
+++ src/utility/util-api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import utils from './util-api';
+import API_BASE_URL from './api-config';
 
 const usersAPI = {
   createUser(username, email, password) {
@@ -15,18 +16,29 @@ const usersAPI = {
   },
 
   loginUser(username, password) {
-    return axios.post('/auth/login')
-      .then((res) => utils.normalizeResponseErrors(res))
-      .then((res) => res.json())
-      .then(({ authToken }) => utils.storeAuthInfo(authToken))
-      .catch((err) => {
-        console.error(err)
-        const status = err;
-        const message = (status === 401) 
-          ? 'Unauthorized login'
-          : 'Incorrect username or password';
-          return message;
-      });
+    // return axios.post(API_BASE_URL + '/auth/login', { username, password})
+    return fetch(`${API_BASE_URL}/auth/login`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+    .then((res) => utils.normalizeResponseErrors(res))
+    .then((res) => res.json())
+    .then(({ authToken }) => utils.storeAuthInfo(authToken))
+    .catch((err) => {
+      console.error(err)
+      const status = err;
+      const message = (status === 401) 
+        ? 'Unauthorized login'
+        : 'Incorrect username or password';
+        return err + message;
+    });
   },
   
 };
