85a92177a2893c172e7e2f0dfdd21dd5b8ff46ba
Cam Prier <CSPrier@gmail.com>
2020-04-18 15:01:25 -0400

    created util-user.js and brought in validateLogin function and fixed import

---
+++ src/utility/util-user.js
@@ -0,0 +1,24 @@
+import UtilUI from '../utility/util-ui';
+
+function validateLogin(values) {
+  let errors = {};
+
+  if (UtilUI.validators.required(values.username) === '*Required') {
+    errors.username = 'Username is required';
+  } 
+  else if (UtilUI.validators.nonEmpty(values.username) === '*Field cannot be blank') {
+    errors.username = 'Field cannot be blank';
+  }
+  else if (UtilUI.validators.isTrimmed(values.username) === '*Cannot start or end with whitespace') {
+    errors.username = 'Cannot start or end with whitespace';
+  }
+
+  if (UtilUI.validators.required(values.password) === '*Required') {
+    errors.password = 'Password is required';
+  }
+  else if (UtilUI.validators.nonEmpty(values.password) === '*Field cannot be blank') {
+    errors.password = 'Password cannot be blank'
+  }
+
+  return errors;
+};
