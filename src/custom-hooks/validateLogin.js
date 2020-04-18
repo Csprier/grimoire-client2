c80232f59e070beff4d9a650d7bc9a8a9d350bd6
Cam Prier <CSPrier@gmail.com>
2020-04-18 14:59:27 -0400

    Fixed validator function Util imports and usage in the conditionals

--- src/custom-hooks/validateLogin.js
+++ src/custom-hooks/validateLogin.js
@@ -1,22 +1,22 @@
-import utils from '../utility/utils';
+import Util from '../utility/Util';
 
 export default function validateLogin(values) {
   let errors = {};
 
-  if (utils.utilUI.validators.required(values.username) === '*Required') {
+  if (Util.UI.validators.required(values.username) === '*Required') {
     errors.username = 'Username is required';
   } 
-  else if (utils.utilUI.validators.nonEmpty(values.username) === '*Field cannot be blank') {
+  else if (Util.UI.validators.nonEmpty(values.username) === '*Field cannot be blank') {
     errors.username = 'Field cannot be blank';
   }
-  else if (utils.utilUI.validators.isTrimmed(values.username) === '*Cannot start or end with whitespace') {
+  else if (Util.UI.validators.isTrimmed(values.username) === '*Cannot start or end with whitespace') {
     errors.username = 'Cannot start or end with whitespace';
   }
 
-  if (utils.utilUI.validators.required(values.password) === '*Required') {
+  if (Util.UI.validators.required(values.password) === '*Required') {
     errors.password = 'Password is required';
   }
-  else if (utils.utilUI.validators.nonEmpty(values.password) === '*Field cannot be blank') {
+  else if (Util.UI.validators.nonEmpty(values.password) === '*Field cannot be blank') {
     errors.password = 'Password cannot be blank'
   }
 
--- src/custom-hooks/validateRegister.js
+++ src/custom-hooks/validateRegister.js
@@ -1,35 +1,35 @@
-import utils from '../utility/utils';
+import Util from '../utility/Util';
 
 export default function validateRegister(values) {
   let errors = {};
 
-  if (utils.utilUI.validators.required(values.username) === '*Required') {
+  if (Util.UI.validators.required(values.username) === '*Required') {
     errors.username = 'Username is required';
   } 
-  else if (utils.utilUI.validators.nonEmpty(values.username) === '*Field cannot be blank') {
+  else if (Util.UI.validators.nonEmpty(values.username) === '*Field cannot be blank') {
     errors.username = 'Field cannot be blank';
   }
-  else if (utils.utilUI.validators.isTrimmed(values.username) === '*Cannot start or end with whitespace') {
+  else if (Util.UI.validators.isTrimmed(values.username) === '*Cannot start or end with whitespace') {
     errors.username = 'Cannot start or end with whitespace';
   }
 
-  if (utils.utilUI.validators.required(values.email) === '*Required') {
+  if (Util.UI.validators.required(values.email) === '*Required') {
     errors.email = 'Email is required';
   }
-  else if (utils.utilUI.validators.nonEmpty(values.email) === '*Field cannot be blank') {
+  else if (Util.UI.validators.nonEmpty(values.email) === '*Field cannot be blank') {
     errors.email = 'Field cannot be blank';
   }
-  else if (utils.utilUI.validators.validEmail(values.email) === '*Invalid email address') {
+  else if (Util.UI.validators.validEmail(values.email) === '*Invalid email address') {
     errors.email = 'Not a valid email address';
   }
-  else if (utils.utilUI.validators.isTrimmed(values.email) === '*Cannot start or end with whitespace') {
+  else if (Util.UI.validators.isTrimmed(values.email) === '*Cannot start or end with whitespace') {
     errors.email = 'Cannot start or end with whitespace';
   }
  
-  if (utils.utilUI.validators.required(values.password) === '*Required') {
+  if (Util.UI.validators.required(values.password) === '*Required') {
     errors.password = 'Password is required';
   }
-  else if (utils.utilUI.validators.nonEmpty(values.password) === '*Field cannot be blank') {
+  else if (Util.UI.validators.nonEmpty(values.password) === '*Field cannot be blank') {
     errors.password = 'Password cannot be blank'
   }
 
