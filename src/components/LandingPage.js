70efc487f93c30f201e737db859cbd6dd226403e
Cam Prier <CSPrier@gmail.com>
2020-04-18 18:01:14 -0400

    added redirect on the landingpage to the dashboard if the authToken persists

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+
+import Util from '../utility/util';
 
 const LandingPage = () => {
+  if (Util.DATA.loadAuthToken()) {
+    return <Redirect to="/dashboard" />
+  }
+
   return(
     <div className="landing-page-container">
       <Link to="/login">Login</Link>
