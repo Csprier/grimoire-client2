d8527335ceaf14652ef6d46f8643e1892506947f
Cam Prier <CSPrier@gmail.com>
2020-04-18 15:02:49 -0400

    Added validateRegister to util-user.js

--- src/utility/util-user.js
+++ src/utility/util-user.js
@@ -23,3 +23,40 @@ function validateLogin(values) {
   return errors;
 };
 
+
+function validateRegister(values) {
+  let errors = {};
+
+  if (UtilUI.validators.required(values.username) === '*Required') {
+    errors.username = 'Username is required';
+  } 
+  else if (UtilUI.validators.nonEmpty(values.username) === '*Field cannot be blank') {
+    errors.username = 'Field cannot be blank';
+  }
+  else if (UtilUI.validators.isTrimmed(values.username) === '*Cannot start or end with whitespace') {
+    errors.username = 'Cannot start or end with whitespace';
+  }
+
+  if (UtilUI.validators.required(values.email) === '*Required') {
+    errors.email = 'Email is required';
+  }
+  else if (UtilUI.validators.nonEmpty(values.email) === '*Field cannot be blank') {
+    errors.email = 'Field cannot be blank';
+  }
+  else if (UtilUI.validators.validEmail(values.email) === '*Invalid email address') {
+    errors.email = 'Not a valid email address';
+  }
+  else if (UtilUI.validators.isTrimmed(values.email) === '*Cannot start or end with whitespace') {
+    errors.email = 'Cannot start or end with whitespace';
+  }
+ 
+  if (UtilUI.validators.required(values.password) === '*Required') {
+    errors.password = 'Password is required';
+  }
+  else if (UtilUI.validators.nonEmpty(values.password) === '*Field cannot be blank') {
+    errors.password = 'Password cannot be blank'
+  }
+
+  return errors;
+};
+
