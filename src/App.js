f96d2d5ca332843a612fb5b335fb4f5c0cacbaf9
Cam Prier <CSPrier@gmail.com>
2020-04-18 17:17:10 -0400

    Created history object and added react-router-dom routers and switch to the App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+// CSS
 import './App.css';
 
+// COMPONENTS
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 
+// History object
+import history from './history';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <Login />
-      {/* <Register /> */}
-      <Dashboard />
-    </div>
+    <Router history={history}>
+      <div className="App">
+        <header className="App-header">
+          Grimoire
+        </header>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
