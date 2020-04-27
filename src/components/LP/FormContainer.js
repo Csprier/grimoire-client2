20079ecf307299fac5028bd754264783fc1f6374
Cam Prier <CSPrier@gmail.com>
2020-04-27 15:52:41 -0400

    Corrected style and animations of form-container

--- src/components/LP/FormContainer.js
+++ src/components/LP/FormContainer.js
@@ -4,25 +4,34 @@ import '../css/css-LP/form-container.css';
 import Login from '../Login';
 import Register from '../Register';
 
-function FormContainer(props) {
+function FormContainer() {
   const [loginMenu, setLoginMenu] = useState(false);
   const [registerMenu, setRegisterMenu] = useState(false);
   return(
-    <div className="form-container">
+    <div className={loginMenu || registerMenu ? "form-container height-animation-class" : "form-container"}>
       
       <div className="animation-button-container">
         <div 
           className="animation-toggle"
-          onClick={() => setLoginMenu(!loginMenu)}
+          onClick={() => {
+            if (registerMenu) {
+              setRegisterMenu(!registerMenu);
+            }
+            setLoginMenu(!loginMenu);
+          }}
         >Login</div>
 
         <div 
           className="animation-toggle"
-          onClick={() => setRegisterMenu(!registerMenu)}
+          onClick={() => {
+            if (loginMenu) {
+              setLoginMenu(!loginMenu);
+            }
+            setRegisterMenu(!registerMenu);
+          }}
         >Register</div>
       </div>
-
-
+      
       <div className={loginMenu ? "login-display login-slide-right" : "login-display"}>
         <Login />
       </div>
