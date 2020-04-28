4c5c4eea3b462fa0c8adab1fbf992f3aaa8de3bf
Cam Prier <CSPrier@gmail.com>
2020-04-28 14:47:31 -0400

    fixed styles on landingpage

--- src/components/LandingPage/LandingPage.styled.js
+++ src/components/LandingPage/LandingPage.styled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 const LandingPageContainer = styled.div`
+  position: absolute;
   width: 100%;
   height: 100%;
   margin: 0;
   padding: 0;
-  background-color: darkgray;
+  background-color: #242526;
 `;
 
 export default LandingPageContainer;
