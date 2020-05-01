7aa91e682860a7c296b5d1f02f1d1bb3d2e4eaf6
Cam Prier <CSPrier@gmail.com>
2020-05-01 15:32:12 -0400

    Created the starting component for the form animation on the landing page

---
+++ src/components/LandingPage/components/AnimatedFormComponent.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function AnimatedFormComponent() {
+  const [activeMenu, setActiveMenu] = useState('');
+  return(
+    
+  );
+};
+
+export default AnimatedFormComponent;
