ac7937380b338a976db84f015b4bea81e66bd846
Cam Prier <CSPrier@gmail.com>
2020-05-06 15:34:26 -0400

    Fixed console warning in Navigation.jsx by adding [] to the useEffect hook. And created onChange and keyCommand handling in the editortest

--- src/components/EditorTest/EditorTest.jsx
+++ src/components/EditorTest/EditorTest.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
 
 import './editor-test.css';
 
 function EditorTest() {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
+  function onChange(editorState) {
+    setEditorState(editorState);
+  }
+
+  function handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      onChange(newState);
+      return 'Handled';
+    }
+    return 'Not handled';
+  };
+
   return(
-    <Editor editorState={editorState} onChange={setEditorState} />
+    <Editor 
+      editorState={editorState} 
+      onChange={onChange}
+      handleKeyCommand={handleKeyCommand}
+    />
   );
 };
 
