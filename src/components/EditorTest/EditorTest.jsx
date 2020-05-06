7e80f8bf7cd11695f23217ccb8328b8641f75ca0
Cam Prier <CSPrier@gmail.com>
2020-05-06 17:35:19 -0400

    Data is persisting in local storage

--- src/components/EditorTest/EditorTest.jsx
+++ src/components/EditorTest/EditorTest.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
-import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
+import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
 
 import './editor-test.css';
 
 function EditorTest() {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  /** Persisted Data */
+  const content = window.localStorage.getItem('content');
+  let setupState = EditorState.createEmpty();
+  if (content) {
+   setupState = EditorState.createWithContent(convertFromRaw(JSON.parse(content)));
+  } 
+ 
+  /** Hook state */
+  let [editorState, setEditorState] = useState(setupState);
+
+  function _saveContent(content) {
+    window.localStorage.setItem('content', JSON.stringify(convertToRaw(content)));
+  }
 
   function onChange(editorState) {
+    const contentState = editorState.getCurrentContent();
+    console.log('content state', convertToRaw(contentState));
+    _saveContent(contentState);
     setEditorState(editorState);
   }
 
