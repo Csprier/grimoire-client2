0aef05de2d7f8cf25451bc938a427915cdee5a9c
Cam Prier <CSPrier@gmail.com>
2020-05-16 21:15:28 -0400

    Cleaned up util-note.js of util-editor functions and moved them and their jsdocs to util-editor.js

--- src/utility/util-editor.js
+++ src/utility/util-editor.js
@@ -1,3 +1,4 @@
+import { RichUtils, convertToRaw } from 'draft-js';
 // onChange
 // getLengthOfSelectedText
 // handleBeforeInput
@@ -7,11 +8,29 @@
 // toggleBlockType
 // toggleInlineStyles
 
-function _onChange() {
-
+function _onChange(editorState) {
+  const contentState = editorState.getCurrentContent();
 };
 
+function handleKeyCommand(command, editorState) {
+  const newState = RichUtils.handleKeyCommand(editorState, command);
+  if (newState) {
+    _onChange(newState);
+    return 'Handled';
+  }
+  return 'Not handled';
+};
+
+
+/** ================================================= */
+/** EXPORTS */
 const UtilEDITOR = {
+  /**
+   * handleKeyCommand: Uses RichUtils to monitor fancy key commands like cmd+b for bold, cmd+i for italic, etc
+   * @param {      }    command - key command
+   * @param {object}    editorState - editor state object
+   */
+  handleKeyCommand: (command, editorState) => handleKeyCommand(command, editorState),
   /**
    * onChange:
    * @param
--- src/utility/util-note.js
+++ src/utility/util-note.js
@@ -1,26 +1,4 @@
 import UtilUI from '../utility/util-ui';
-import { RichUtils, convertToRaw } from 'draft-js';
-
-/** EDITOR FUNCTIONS */
-function handleKeyCommand(command, editorState) {
-  const newState = RichUtils.handleKeyCommand(editorState, command);
-  if (newState) {
-    _onEditorChange(newState);
-    return 'Handled';
-  }
-  return 'Not handled';
-};
-
-function _onEditorChange(editorState, setEditorState, handleContent) {
-  const contentState = editorState.getCurrentContent();
-  console.log('contentState OEC: ', convertToRaw(contentState));
-  // setEditorState(contentState);
-  // handleContent(contentJSON);
-};
-
-function _saveContent(content) {
-  window.localStorage.setItem('content', JSON.stringify(convertToRaw(content)));
-}
 
 /** FORM VALIDATIONS */
 function _validateNote(values) {
@@ -32,28 +10,9 @@ function _validateNote(values) {
   return errors;
 };
 
-/**
- * EXPORTS
- */
+/** ================================================= */
+/** EXPORTS */
 const UtilNOTE = {
-  /**
-   * handleKeyCommand: Uses RichUtils to monitor fancy key commands like cmd+b for bold, cmd+i for italic, etc
-   * @param {      }    command - key command
-   * @param {object}    editorState - editor state object
-   */
-  handleKeyCommand: (command, editorState) => handleKeyCommand(command, editorState),
-  /**
-   * onEditorChange: Gets current content state, saves content to local storage, and uses handleContent hook function
-   * @param {object}    editorState - editor state object
-   * @param {function}  setEditorState - set editor state hook function
-   * @param {function}  handleContent - useForm custom function for modifying the value of content in the form state
-   */
-  onEditorChange: (editorState,  setEditorState, handleContent) => _onEditorChange(editorState,  setEditorState, handleContent),
-  /**
-   * saveContent: saves content to local storage
-   * @param {string}    content - JSON string
-   */
-  saveContent: values => _saveContent(values),
   /**
    * validateNote: Validation for modifying Notes
    * @param {object}    values - form values
