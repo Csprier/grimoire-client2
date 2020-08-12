ffa2f9664760718e7f66b765dffdd5a021073abb
Cam Prier <CSPrier@gmail.com>
2020-08-12 14:25:12 -0400

    Troubleshooting the conditional render of which editor is rendered which is showing the opposite component temporarily

--- src/components/DesktopView/components/DVEditorDisplay.styled.js
+++ src/components/DesktopView/components/DVEditorDisplay.styled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 const DVEditorContainer = styled.div`
+  display: ${props => props.note ? 'none' : 'block'};
   width: 100%;
   height: ${props => props.animate ? '600px' : '0px'};
   margin: 0;
   padding: 0;
-  // border: 1px solid blue;
+  border: 1px solid lightgray;
   transition-property: height;
   transition-duration: .1s;
   transition-delay: .1s;
