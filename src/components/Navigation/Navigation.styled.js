99e8a0aa2da79b5909bee5a061952348579228e0
Cam Prier <CSPrier@gmail.com>
2020-08-18 18:25:58 -0400

    added new logouticon and fixed hover state in Navigation

--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -20,30 +20,17 @@ const UsernameHeader = styled.h3`
   color: white;
 `;
 
-const LogOutButton = styled.button`
-  width: 80px;
-  height: 100%;
-  margin: 0px 10px 0px 0px;
-  padding: 6px 8px;
-  font-size: 14px;
-  text-align: center;
-  color: white;
-  background-color: black;
-  border: 1px solid black;
-  &:focus { outline: none; }
-  &:hover { 
-    background-color: darkslateblue; 
-    color: goldenrod;
-  }
-`;
-
 const LogOutIcon = styled.img`
   display: flex;
   align-self: center;
-  width: 28px;
-  height: 25px;
-  margin: 0px 5px 0px 0px;
+  width: 30px;
+  margin: 0px 10px 0px 0px;
   padding: 0;
+  filter: invert(1);
+  &:focus { outline: none; }
+  &:hover {
+    filter: invert(66%) sepia(94%) saturate(420%) hue-rotate(356deg) brightness(90%) contrast(89%);
+  }
 `;
 
 const NavigationButtonContainer = styled.div`
@@ -73,7 +60,6 @@ export {
   IconButton,
   Navigation,
   NavigationButtonContainer,
-  LogOutButton,
   LogOutIcon,
   UsernameHeader
 };
