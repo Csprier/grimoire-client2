4a80469cb3e03e39b3fdf7d67f6fdbfb2db4ed20
Cam Prier <CSPrier@gmail.com>
2020-08-19 23:59:29 -0400

    The toolbar works! But it is not styled or optimized in the file structure yet

--- src/components/DesktopView/components/Editor/DVAddNoteTextEditor.jsx
+++ src/components/DesktopView/components/Editor/DVAddNoteTextEditor.jsx
@@ -1,16 +1,80 @@
 import React, { Component } from 'react';
-import { 
-  convertToRaw,
-  Editor,
-  EditorState,
-  RichUtils
-} from 'draft-js';
 
 /** Util */
 import Util from '../../../../utility/util';
 
 /** Styles */
 import './dv-note-text-editor.css';
+
+import { 
+  convertToRaw,
+  EditorState,
+  RichUtils
+} from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import createToolbarPlugin, { Separator }  from 'draft-js-static-toolbar-plugin';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from 'draft-js-buttons';
+const toolbarPlugin = createToolbarPlugin();
+const { Toolbar } = toolbarPlugin;
+const plugins = [toolbarPlugin];
+
+class HeadlinesPicker extends Component {
+  componentDidMount() {
+    setTimeout(() => { 
+      window.addEventListener('click', this.onWindowClick); 
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onWindowClick);
+  }
+
+  // Call `onOverrideContent` again with `undefined`
+  // so the toolbar can show its regular content again.
+  _onWindowClick = () => this.props.onOverrideContent(undefined);
+
+  render() {
+    const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
+    return (
+      <div>
+        {buttons.map((Button, i) => // eslint-disable-next-line
+          // <Button key={i} {...this.props} className="tb-button" />
+          <div key={i} {...this.props} className="tb-button"></div>
+        )}
+      </div>
+    );
+  }
+};
+
+class HeadlinesButton extends Component {
+  onClick = () =>
+    // A button can call `onOverrideContent` to replace the content
+    // of the toolbar. This can be useful for displaying sub
+    // menus or requesting additional information from the user.
+    this.props.onOverrideContent(HeadlinesPicker);
+
+  render() {
+    return (
+      <div className="headlineButtonWrapper">
+        <button onClick={this.onClick} className="headlineButton">
+          H
+        </button>
+      </div>
+    );
+  }
+};
 
 class AddNoteTextEditor extends Component {
   constructor() {
@@ -21,9 +85,8 @@ class AddNoteTextEditor extends Component {
     this.onChange = this.onChange.bind(this);
     this._saveContent = this._saveContent.bind(this);
     this._handleKeyCommand = this._handleKeyCommand.bind(this);
-    // this._onBoldClick = this._onBoldClick.bind(this);
   };
-
+  
   /**
    * onChange: when an onChange event happens, get the contentState of the editorState, 
    *  pass the raw JSON into props.handleContentChange and save it to localStorage via _saveContent, 
@@ -63,31 +126,38 @@ class AddNoteTextEditor extends Component {
     return 'not-handled';
   };
 
-  /** */
-  // _onBoldClick = () => {
-  //   const { editorState } = this.state;
-  //   this.onChange(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
-  // }
+  /**  */
+  focus = () => {
+    this.editor.focus();
+  };
 
   render() {
     const { editorState } = this.state;
     return(
-      <div>
-        {/* <div className="editor-button-container">
-          <button 
-            className="editor-button"
-            type="button" 
-            onClick={() => this._onBoldClick()}
-          >
-            <strong>BOLD</strong>
-          </button>
-
-        </div> */}
+      <div onClick={this.focus}>
+        <Toolbar>
+          {(externalProps) => (
+            <div className="flex-fragment">
+              <BoldButton {...externalProps} />
+              <ItalicButton {...externalProps} />
+              <UnderlineButton {...externalProps} />
+              <CodeButton {...externalProps} />
+              <Separator {...externalProps} />
+              <HeadlinesButton {...externalProps} />
+              <UnorderedListButton {...externalProps} />
+              <OrderedListButton {...externalProps} />
+              <BlockquoteButton {...externalProps} />
+              <CodeBlockButton {...externalProps} />
+            </div>
+          )}
+        </Toolbar>
 
         <Editor 
           editorState={editorState}
           onChange={this.onChange}
           handleKeyCommand={this._handleKeyCommand}
+          plugins={plugins}
+          ref={(element) => { this.editor = element; }}
         />
       </div>
     );
