6ce2521c5df5ac97dc697b8b847d1f3d39e49067
Cam Prier <CSPrier@gmail.com>
2020-11-11 20:17:37 -0500

    Added SearchNotes functionality

--- src/components/ResponsiveView/ResponsiveViewComponent.jsx
+++ src/components/ResponsiveView/ResponsiveViewComponent.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 /** Components */
 import Note from './components/Note';
+import SearchNotes from '../SearchNotes/SearchNotes';
 
 /** Styles */
 import './responsive-view.css';
@@ -9,25 +10,43 @@ import './responsive-view.css';
 const ResponsiveViewComponent = (props) => {
   const [notes, setNotes] = useState([]);
   const [selectedNote, setAsSelectedNote] = useState({});
-
+  const { searchTerm } = props;
+  
   useEffect(() => {
     setNotes(props.notes);
     console.log('selectedNote:', selectedNote);
   }, [props.notes, setNotes, notes, selectedNote]);
 
+  /** ================================================================================ */
+  /** SEARCH TERM FILTER 
+  /** ================================= */
+  let filteredListOfNotes = notes.filter(note => note.title.includes(searchTerm))
+  let listOfNotesToRender = notes;
+
+  if (searchTerm !== '') {
+    listOfNotesToRender = filteredListOfNotes;
+  } else {
+    listOfNotesToRender = notes;
+  };
+
   return(
     <div className="responsive-view-container">
       <div className="responsive-view">
 
+        <div className="dashboard-util-container">
+          <SearchNotes 
+            notes={props.notes}
+            setSearchTerm={props.setSearchTerm} 
+          />
+        </div>
         <ul className="note-list">
-          {notes.map(note => {
+          {listOfNotesToRender.map(note => {
             return(
               <Note 
                 note={note} 
                 key={note._id}
                 setAsSelectedNote={setAsSelectedNote}
                 selectedNote={selectedNote}
-                // closeNoteEditor={closeNoteEditor}
                 reRenderFunction={props.reRenderFunction}
               />
             )
