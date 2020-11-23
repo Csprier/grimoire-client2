7e93528e51eca21149c7c9bc6a16aa4fe178d613
Cam Prier <CSPrier@gmail.com>
2020-11-23 14:18:12 -0500

    Prep for desktopview deployment

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -11,9 +11,9 @@ import {
 
 /** Components */
 import NavigationBar from '../Navigation/Navigation';
-// import DesktopViewComponent from '../DesktopView/DesktopViewComponent';
+import DesktopViewComponent from '../DesktopView/DesktopViewComponent';
 // import MobileViewComponent from '../MobileView/MobileViewComponent';
-import ResponsiveViewComponent from '../ResponsiveView/ResponsiveViewComponent';
+// import ResponsiveViewComponent from '../ResponsiveView/ResponsiveViewComponent';
 
 /** Force rerender */
 const useForceUpdate = () => useState()[1]; 
@@ -63,13 +63,13 @@ const Dashboard = () => {
       <NavigationBar />
 
       <DashboardContent>
-        <ResponsiveViewComponent 
+        {/* <ResponsiveViewComponent 
           notes={listOfNotes}
           reRenderFunction={_reRenderNoteList}
           searchTerm={searchTerm}
           setSearchTerm={setSearchTerm} 
-        />
-        {/* {(viewWidth >= 700) 
+        /> */}
+        {(viewWidth >= 700) 
           ? <DesktopViewComponent 
               notes={listOfNotes} 
               reRenderFunction={_reRenderNoteList}
@@ -81,7 +81,7 @@ const Dashboard = () => {
                 reRenderFunction={_reRenderNoteList}
                 searchTerm={searchTerm}
                 setSearchTerm={setSearchTerm} 
-              />} */}
+              />}
 
       </DashboardContent>
 
